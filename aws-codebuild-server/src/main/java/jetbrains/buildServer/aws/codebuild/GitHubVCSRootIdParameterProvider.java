5500299d911a940d8f3b50e72a160ff415cc32ab
Victory Petrenko <vbedrosova@gmail.com>
2020-07-02 15:48:10 +0200

    TW-66675
    (cherry picked from commit 82bca6403b1cc2fb4f0d9952523f85d845bd02be)

--- aws-codebuild-server/src/main/java/jetbrains/buildServer/aws/codebuild/GitHubVCSRootIdParameterProvider.java
+++ aws-codebuild-server/src/main/java/jetbrains/buildServer/aws/codebuild/GitHubVCSRootIdParameterProvider.java
@@ -17,6 +17,7 @@
 package jetbrains.buildServer.aws.codebuild;
 
 import jetbrains.buildServer.serverSide.SBuild;
+import jetbrains.buildServer.serverSide.impl.BuildTypeImpl;
 import jetbrains.buildServer.serverSide.parameters.AbstractBuildParametersProvider;
 import jetbrains.buildServer.vcs.VcsRootInstanceEntry;
 import jetbrains.buildServer.vcs.impl.VcsRootInstanceImpl;
@@ -32,10 +33,12 @@ public class GitHubVCSRootIdParameterProvider extends AbstractBuildParametersPro
   @NotNull
   @Override
   public Map<String, String> getParameters(@NotNull SBuild build, boolean emulationMode) {
-    for (VcsRootInstanceEntry e : build.getVcsRootEntries()) {
-      if ("jetbrains.git".equals(e.getVcsName()) && e.getProperties().get("url").contains("github.com")) {
-        final String vcsRootId = e.getVcsRoot() instanceof VcsRootInstanceImpl ? ((VcsRootInstanceImpl)e.getVcsRoot()).getExternalId() : CodeBuildConstants.UNKNOWN_GIT_HUB_VCS_ROOT_ID;
-        return Collections.singletonMap(CodeBuildConstants.GIT_HUB_VCS_ROOT_ID_CONFIG_PARAM, vcsRootId);
+    if (!BuildTypeImpl.isInsideVcsRootsResolving()) {
+      for (VcsRootInstanceEntry e : build.getVcsRootEntries()) {
+        if ("jetbrains.git".equals(e.getVcsName()) && e.getProperties().get("url").contains("github.com")) {
+          final String vcsRootId = e.getVcsRoot() instanceof VcsRootInstanceImpl ? ((VcsRootInstanceImpl) e.getVcsRoot()).getExternalId() : CodeBuildConstants.UNKNOWN_GIT_HUB_VCS_ROOT_ID;
+          return Collections.singletonMap(CodeBuildConstants.GIT_HUB_VCS_ROOT_ID_CONFIG_PARAM, vcsRootId);
+        }
       }
     }
     return Collections.emptyMap();
